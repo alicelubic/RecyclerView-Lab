6b18eac5004882a918c2f9d4435148c91d8eea1f
Alice Lubic <lubicalice@gmail.com>
2016-07-05 18:03:10 -0400

    what do i do in the bindviewholder??

--- starter-code/RecyclerViewLab/app/src/main/java/example/com/recyclerviewlab/CustomRecyclerViewAdapter2.java
+++ starter-code/RecyclerViewLab/app/src/main/java/example/com/recyclerviewlab/CustomRecyclerViewAdapter2.java
@@ -4,11 +4,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * Created by owlslubic on 7/5/16.
  */
 public class CustomRecyclerViewAdapter2 extends RecyclerView.Adapter<CustomViewHolder2> {
+
     @Override
     public CustomViewHolder2 onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
@@ -20,6 +24,8 @@ public class CustomRecyclerViewAdapter2 extends RecyclerView.Adapter<CustomViewH
 
     @Override
     public void onBindViewHolder(CustomViewHolder2 holder, int position) {
+
+
 
     }
 
